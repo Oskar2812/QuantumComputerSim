50bbc35c71ec0d41e37e5cefb6b6313a0b51f272
Oskar2812 <oskar.grewe@gmail.com>
2024-04-11 14:06:21 +0100

    Added X logic gates Added a system by which single qubit logic gates can act on the state and implemented the X (not) gate

--- include/QComputer.h
+++ include/QComputer.h
@@ -26,6 +26,10 @@ class QComputer{
         /// @brief Prints the current state vector
         void showState() const;
 
+        /// @brief Acts a not gate on the nn'th qubit 
+        /// @param qubit The qubit to act on
+        void actX(int index);
+
     private:
         /// @brief Number of qubits in state, TODO: Add a register class and stick it inside there
         int nQubits;
@@ -35,6 +39,10 @@ class QComputer{
         /// @brief Initialises a qubit
         /// @return A 2d vector representing the qubit
         Eigen::Vector2d initQubit(int initState) const;
+
+        /// @brief The method actually building the matrix that cts on the final state
+        /// @param gate The matrix to be used
+        void act(Eigen::Matrix2d gate, int index);
 
 
 };
--- src/QComputer.cpp
+++ src/QComputer.cpp
@@ -1,6 +1,8 @@
 #include "../include/QComputer.h"
 
 #include <iostream>
+#include <bitset>
+#include <string>
 
 //Eigen includes
 #include <unsupported/Eigen/KroneckerProduct>
@@ -39,5 +41,40 @@ QComputer::QComputer(int nQ, int initState) : nQubits(nQ){
 }
 
 void QComputer::showState() const{
-    std::cout << "Number of qubits: " << nQubits << "\n" << state << std::endl;
+
+    std::cout << "Number of qubits: " << nQubits << std::endl;
+    for( int ii = 0; ii < state.size(); ii++){
+        std::bitset<32> binary(ii);
+
+        std::string str = binary.to_string();
+        str = str.substr(str.size() - nQubits, nQubits);
+        std::cout << state[ii] << "      |" << str << ">\n"; 
+    }
+}
+void QComputer::act(Matrix2d gate, int index){
+    if(index < 0 || nQubits <= index){
+        throw std::invalid_argument("Invalid qubit index: Index must be between 0 and " + std::to_string(nQubits - 1));
+    }
+
+    MatrixXd result = MatrixXd::Identity(1,1);
+
+    for(int ii = 0; ii < nQubits; ii++){
+        if(ii == index){
+            result = kroneckerProduct(result, gate).eval();
+        } else {
+            result = kroneckerProduct(result, MatrixXd::Identity(2,2)).eval();
+        }
+    }
+
+    state = result * state;
+}
+
+void QComputer::actX(int nn){
+
+    Matrix2d X;
+    X << 0, 1,
+         1, 0;      
+
+    act(X, nn);
+    
 }
